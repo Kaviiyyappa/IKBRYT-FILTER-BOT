e544cb5e09147dd73c2df57310d3f8cfca3acfa4
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2025-11-24 15:26:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,9 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @IKBRYT {file_name}
-
-<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
+    CAPTION = 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
@@ -752,6 +750,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
